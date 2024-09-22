c1a6f329fb343629c77a20cd1518d5166467daff
nup chan <bao96065@gmail.com>
2024-09-22 12:02:11 +0700

    chore: change admin page color and port
    đổi màu bên admin thành màu cam và đổi sang sử dụng port của server là 5000, user là 5173 và admin là 5174

--- user/src/Login.jsx
+++ user/src/Login.jsx
@@ -9,7 +9,7 @@ const Login = ({ onLogin }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/login', {
+    const response = await fetch('http://localhost:5000/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

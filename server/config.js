5cbb4d9fa9b696009d2820190b5c1153e56ca042
The Pink Cat <asdsaphat@gmail.com>
2024-10-22 22:57:18 +0700

    them validation cho booking

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   orderInfo: 'pay with MoMo',
   partnerCode: 'MOMO',
   redirectUrl: 'http://localhost:5173/payment-result',
-  ipnUrl: 'https://b986-2001-ee0-25c-4f63-b4d3-c099-64ff-9148.ngrok-free.app/callback', //chú ý: cần dùng ngrok thì momo mới post đến url này được
+  ipnUrl: 'https://a158-2405-4802-9190-77c0-e9e6-6677-8b0f-5252.ngrok-free.app/callback', //chú ý: cần dùng ngrok thì momo mới post đến url này được
   requestType: 'payWithMethod',
   extraData: '',
   orderGroupId: '',

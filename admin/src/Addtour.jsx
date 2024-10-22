478f2f407cec0eeb183d35ca201bd39b06047565
Tequinzyy <tonyminhquan450@gmail.com>
2024-10-22 19:24:20 +0700

    rename AddTour

--- admin/src/Addtour.jsx
+++ admin/src/Addtour.jsx
@@ -1,134 +1,148 @@
-import { useState, useEffect } from 'react';
-import { Box, TextField, Button, MenuItem } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import './AddTour.css'
+import { useState, useEffect } from "react";
+import { Box, TextField, Button, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "./AddTour.css";
 
 const AddTour = () => {
     const [tour, setTour] = useState({
-        tentour: '',
-        loaitour: 'Tour trong nước',
-        gia: '',
-        sove: '',
-        hinhanh: '',
-        mota: '',
-        khoihanh: '',
-        trangthai: 'Còn vé',
-        idlichtrinh: '',
-        phuongtiendichuyen: 'Xe Buýt'
+        tentour: "",
+        loaitour: "Tour trong nước",
+        gia: "",
+        sove: "",
+        hinhanh: "",
+        mota: "",
+        khoihanh: "",
+        trangthai: "Còn vé",
+        idlichtrinh: "",
+        phuongtiendichuyen: "Xe Buýt",
     });
 
     const [schedules, setSchedules] = useState([]);
-    const [imagePreview, setImagePreview] = useState('');
+    const [imagePreview, setImagePreview] = useState("");
     const [errors, setErrors] = useState({
-        tentour: '',
-        loaitour: '',
-        gia: '',
-        sove: '',
-        hinhanh: '',
-        mota: '',
-        khoihanh: '',
-        trangthai: '',
-        idlichtrinh: '',
-        phuongtiendichuyen: ''
+        tentour: "",
+        loaitour: "",
+        gia: "",
+        sove: "",
+        hinhanh: "",
+        mota: "",
+        khoihanh: "",
+        trangthai: "",
+        idlichtrinh: "",
+        phuongtiendichuyen: "",
     });
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetch('http://localhost:5000/schedules')
-            .then(response => response.json())
-            .then(data => setSchedules(data))
-            .catch(err => console.error('Error fetching schedules:', err));
+        fetch("http://localhost:5000/schedules")
+            .then((response) => response.json())
+            .then((data) => setSchedules(data))
+            .catch((err) => console.error("Error fetching schedules:", err));
     }, []);
 
     const checkTourExists = async (tentour) => {
         try {
-          const response = await fetch(`http://localhost:5000/check-tour-exists?tentour=${encodeURIComponent(tentour)}`);
-          const data = await response.json();
-          return data.exists;
+            const response = await fetch(
+                `http://localhost:5000/check-tour-exists?tentour=${encodeURIComponent(
+                    tentour
+                )}`
+            );
+            const data = await response.json();
+            return data.exists;
         } catch (err) {
-          console.error('Error checking if tour exists:', err);
-          return false;
+            console.error("Error checking if tour exists:", err);
+            return false;
         }
     };
 
     const validateTentour = (value) => {
         if (!value.trim()) {
-            return 'Trường bắt buộc nhập';
+            return "Trường bắt buộc nhập";
         } else if (/^\d+$/.test(value)) {
-            return 'Tên tour không được chứa toàn số';
+            return "Tên tour không được chứa toàn số";
         }
-        return '';
+        return "";
     };
 
     const validateGia = (value) => {
         const isNumber = /^[0-9]*\.?[0-9]+$/.test(value);
         if (!isNumber || !value.trim()) {
-            return 'Giá phải là một số hợp lệ và không chứa ký tự đặc biệt';
+            return "Giá phải là một số hợp lệ và không chứa ký tự đặc biệt";
         } else if (value <= 0) {
-            return 'Giá phải lớn hơn 0';
+            return "Giá phải lớn hơn 0";
         }
-        return '';
+        return "";
     };
 
     const validateSove = (value) => {
         const isNumber = /^[0-9]+$/.test(value);
         if (!isNumber || !value.trim()) {
-            return 'Số vé phải là một số hợp lệ và không chứa ký tự đặc biệt';
+            return "Số vé phải là một số hợp lệ và không chứa ký tự đặc biệt";
         } else if (parseInt(value, 10) <= 0) {
-            return 'Số vé phải lớn hơn 0';
+            return "Số vé phải lớn hơn 0";
         }
-        return '';
+        return "";
     };
 
     const validateRequiredField = (value, fieldName) => {
-        if (typeof value !== 'string') {
+        if (typeof value !== "string") {
             value = String(value);
         }
 
         if (!value.trim()) {
-            return fieldName === 'idlichtrinh' ? 'Không được để trống' : 'Trường bắt buộc nhập';
+            return fieldName === "idlichtrinh"
+                ? "Không được để trống"
+                : "Trường bắt buộc nhập";
         }
-        return '';
+        return "";
     };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setTour(prev => ({ ...prev, [name]: value }));
+        setTour((prev) => ({ ...prev, [name]: value }));
 
-        let error = '';
-        if (name === 'tentour') {
+        let error = "";
+        if (name === "tentour") {
             error = validateTentour(value);
-        } else if (name === 'gia') {
+        } else if (name === "gia") {
             error = validateGia(value);
-        } else if (name === 'sove') {
+        } else if (name === "sove") {
             error = validateSove(value);
-        } else if (['mota', 'khoihanh', 'idlichtrinh', 'phuongtiendichuyen', 'trangthai'].includes(name)) {
+        } else if (
+            [
+                "mota",
+                "khoihanh",
+                "idlichtrinh",
+                "phuongtiendichuyen",
+                "trangthai",
+            ].includes(name)
+        ) {
             error = validateRequiredField(value, name);
         }
 
-        setErrors(prev => ({ ...prev, [name]: error }));
+        setErrors((prev) => ({ ...prev, [name]: error }));
     };
 
     const handleImageUpload = (e) => {
         const file = e.target.files[0];
         if (!file) {
-            setErrors(prev => ({ ...prev, hinhanh: 'Trường bắt buộc nhập' }));
+            setErrors((prev) => ({ ...prev, hinhanh: "Trường bắt buộc nhập" }));
         } else {
-            setErrors(prev => ({ ...prev, hinhanh: '' }));
+            setErrors((prev) => ({ ...prev, hinhanh: "" }));
             const previewUrl = URL.createObjectURL(file);
             setImagePreview(previewUrl);
 
             const formData = new FormData();
-            formData.append('image', file);
-            fetch('http://localhost:5000/upload-image', {
-                method: 'POST',
+            formData.append("image", file);
+            fetch("http://localhost:5000/upload-image", {
+                method: "POST",
                 body: formData,
             })
-            .then(response => response.json())
-            .then(data => {
-                setTour(prev => ({ ...prev, hinhanh: data.path }));
-            })
-            .catch(err => console.error('Error uploading image:', err));
+                .then((response) => response.json())
+                .then((data) => {
+                    setTour((prev) => ({ ...prev, hinhanh: data.path }));
+                })
+                .catch((err) => console.error("Error uploading image:", err));
         }
     };
 
@@ -137,12 +151,15 @@ const AddTour = () => {
             tentour: validateTentour(tour.tentour),
             gia: validateGia(tour.gia),
             sove: validateSove(tour.sove),
-            mota: validateRequiredField(tour.mota, 'mota'),
-            khoihanh: validateRequiredField(tour.khoihanh, 'khoihanh'),
-            trangthai: validateRequiredField(tour.trangthai, 'trangthai'),
-            idlichtrinh: validateRequiredField(tour.idlichtrinh, 'idlichtrinh'),
-            phuongtiendichuyen: validateRequiredField(tour.phuongtiendichuyen, 'phuongtiendichuyen'),
-            hinhanh: validateRequiredField(tour.hinhanh, 'hinhanh'),
+            mota: validateRequiredField(tour.mota, "mota"),
+            khoihanh: validateRequiredField(tour.khoihanh, "khoihanh"),
+            trangthai: validateRequiredField(tour.trangthai, "trangthai"),
+            idlichtrinh: validateRequiredField(tour.idlichtrinh, "idlichtrinh"),
+            phuongtiendichuyen: validateRequiredField(
+                tour.phuongtiendichuyen,
+                "phuongtiendichuyen"
+            ),
+            hinhanh: validateRequiredField(tour.hinhanh, "hinhanh"),
         };
         return formErrors;
     };
@@ -151,40 +168,45 @@ const AddTour = () => {
         e.preventDefault();
         const formErrors = validateFields();
 
-        if (Object.values(formErrors).every(error => !error)) {
+        if (Object.values(formErrors).every((error) => !error)) {
             const isTourExists = await checkTourExists(tour.tentour);
 
             if (isTourExists) {
-                setErrors(prev => ({ ...prev, tentour: 'This tour already exists' }));
+                setErrors((prev) => ({
+                    ...prev,
+                    tentour: "This tour already exists",
+                }));
             } else {
                 // Proceed to add tour
-                fetch('http://localhost:5000/add-tour', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
+                fetch("http://localhost:5000/add-tour", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(tour),
                 })
-                .then(response => {
-                    if (!response.ok) {
-                        return response.json().then(err => {
-                            throw new Error(err.message);
-                        });
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log('Tour added:', data);
-                    navigate('/');
-                })
-                .catch(err => {
-                    console.error('Error adding tour:', err);
-                    setErrors(prev => ({ ...prev, tentour: err.message }));
-                });
+                    .then((response) => {
+                        if (!response.ok) {
+                            return response.json().then((err) => {
+                                throw new Error(err.message);
+                            });
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log("Tour added:", data);
+                        navigate("/");
+                    })
+                    .catch((err) => {
+                        console.error("Error adding tour:", err);
+                        setErrors((prev) => ({
+                            ...prev,
+                            tentour: err.message,
+                        }));
+                    });
             }
         } else {
             setErrors(formErrors);
         }
     };
-
 
     return (
         <Box className="form-container">
@@ -208,7 +230,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.loaitour || ''}
+                    value={tour.loaitour || ""}
                     error={!!errors.loaitour}
                     helperText={errors.loaitour}
                     className="text-field"
@@ -224,7 +246,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.gia || ''}
+                    value={tour.gia || ""}
                     error={!!errors.gia}
                     helperText={errors.gia}
                     className="text-field"
@@ -236,7 +258,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.sove || ''}
+                    value={tour.sove || ""}
                     error={!!errors.sove}
                     helperText={errors.sove}
                     className="text-field"
@@ -247,7 +269,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.mota || ''}
+                    value={tour.mota || ""}
                     error={!!errors.mota}
                     helperText={errors.mota}
                     className="text-field"
@@ -259,7 +281,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.khoihanh || ''}
+                    value={tour.khoihanh || ""}
                     error={!!errors.khoihanh}
                     helperText={errors.khoihanh}
                     className="text-field"
@@ -271,7 +293,7 @@ const AddTour = () => {
                     name="trangthai"
                     fullWidth
                     onChange={handleChange}
-                    value={tour.trangthai || ''}
+                    value={tour.trangthai || ""}
                     className="text-field"
                 >
                     <MenuItem value="Còn vé">Còn vé</MenuItem>
@@ -285,12 +307,12 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.idlichtrinh || ''}
+                    value={tour.idlichtrinh || ""}
                     error={!!errors.idlichtrinh}
                     helperText={errors.idlichtrinh}
                     className="text-field"
                 >
-                    {schedules.map(schedule => (
+                    {schedules.map((schedule) => (
                         <MenuItem key={schedule.ID} value={schedule.ID}>
                             {schedule.tenlichtrinh}
                         </MenuItem>
@@ -304,7 +326,7 @@ const AddTour = () => {
                     fullWidth
                     onChange={handleChange}
                     required
-                    value={tour.phuongtiendichuyen || ''}
+                    value={tour.phuongtiendichuyen || ""}
                     error={!!errors.phuongtiendichuyen}
                     helperText={errors.phuongtiendichuyen}
                     className="text-field"
@@ -342,7 +364,7 @@ const AddTour = () => {
                 <Button
                     variant="outlined"
                     color="secondary"
-                    onClick={() => navigate('/')}
+                    onClick={() => navigate("/")}
                 >
                     Hủy
                 </Button>

4d2e32ed38613e80b0907a384b46178c691bf174
The Pink Cat <asdsaphat@gmail.com>
2024-10-23 14:05:50 +0700

    merge main?

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
   orderInfo: 'pay with MoMo',
   partnerCode: 'MOMO',
   redirectUrl: 'http://localhost:5173/payment-result',
-  ipnUrl: 'https://6f7f-14-161-49-158.ngrok-free.app/callback', //chú ý: cần dùng ngrok thì momo mới post đến url này được
+  ipnUrl: 'https://a2ce-2001-ee0-25e-157a-912c-8cb9-71bb-dc68.ngrok-free.app/callback', //chú ý: cần dùng ngrok thì momo mới post đến url này được
   requestType: 'payWithMethod',
   extraData: '',
   orderGroupId: '',
